f8575f4d054b52f93dee205c9dd9a79b6f0b7866
Tim McNamara <code@timmcnamara.co.nz>
2010-11-09 12:17:01 +1300

    Moved from distutils to distribute
    Setup.py should now work properly. Before it was being irritating
    and not including the source files (most likely because of my
    lack of experience. Seems to work fine now.

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from sys import version
-if version < '2.2.3':
-    from distutils.dist import DistributionMetadata
-    DistributionMetadata.classifiers = None
-    DistributionMetadata.download_url = None
-
+from setuptools import setup, find_packages
 
 setup(name='slate',
-      version='0.2.2',
+      version='0.2.3',
       description='Extract text from PDF documents easily.',
       author='Tim McNamara',
       author_email='paperless@timmcnamara.co.nz',
       keywords=('pdf', 'text', 'text-extraction'),
       license = "GPL v3 or later",
-      packages=['slate'],
-      package_dir={'slate': 'src/slate'},
+      exclude_package_data={'': ['.gitignore']},
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       requires=['pdfminer'],
+      install_requires=['distribute'],
       classifiers= [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -28,4 +24,5 @@ setup(name='slate',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing',
         'Topic :: Utilities'],
-      long_description = open('README').read())
+      long_description = open('README').read(),
+      url='http://pypi.python.org/pypi/slate')
