6d2e9e4eac6f0921551d9567ee37d638535a525c
Chris Bailey <c.bailey@bristol.ac.uk>
2013-11-14 10:56:54 +0000

    Updating code to conform to new document API

--- src/slate/slate.py
+++ src/slate/slate.py
@@ -6,6 +6,7 @@ from pdfminer.pdfinterp import PDFPageInterpreter as PI
 from pdfminer.pdfdevice import PDFDevice
 from pdfminer.converter import TextConverter
 from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
 
 import utils
 
@@ -34,16 +35,15 @@ class PDFPageInterpreter(PI):
 class PDF(list):
     def __init__(self, file, password='', just_text=1):
         self.parser = PDFParser(file)
-        self.doc = PDFDocument()
+        self.doc = PDFDocument(self.parser)
         self.parser.set_document(self.doc)
-        self.doc.set_parser(self.parser)
         self.doc.initialize(password)
         if self.doc.is_extractable:
             self.resmgr = PDFResourceManager()
             self.device = TextConverter(self.resmgr, outfp=StringIO())
             self.interpreter = PDFPageInterpreter(
                self.resmgr, self.device)
-            for page in self.doc.get_pages():
+            for page in PDFPage.create_pages(self.doc):
                 self.append(self.interpreter.process_page(page))
             self.metadata = self.doc.info
         if just_text:
