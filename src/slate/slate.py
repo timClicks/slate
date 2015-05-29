06a1d3bb45a27fa4dae395af9747f4f8e4400532
Christopher Schmidt <pecos@sqleb.de>
2015-05-29 23:50:47 +0200

    Added ability to change margin parameters

--- src/slate/slate.py
+++ src/slate/slate.py
@@ -33,7 +33,6 @@ class PDFPageInterpreter(PI):
         else:
             ctm = (1,0,0,1, -x0,-y0)
         self.device.outfp.seek(0)
-        #self.device.outfp.buf = ''
         self.device.begin_page(page, ctm)
         self.render_contents(page.resources, page.contents, ctm=ctm)
         self.device.end_page(page)
