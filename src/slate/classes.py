d91b5a6d7848815972273f3c31fa82b6b605d272
Joel Steres <joel@ymobility.com>
2015-11-05 08:30:15 -0800

    Fix text carrier-over between pages. Fix tests.
    1. Fixed tests by including newlines from extracted documents
    2. Added test to illustrate merge bug from commit
    43614f2716fb6020d31bfde64712636b084c417b in which uncleared buffer
    carries text between pages
    3. Added clearing of buffer that also addresses issue #21 in the prior
    codebase

--- src/slate/classes.py
+++ src/slate/classes.py
@@ -41,6 +41,7 @@ class PDFPageInterpreter(PI):
         else:
             ctm = (1,0,0,1, -x0,-y0)
         self.device.outfp.seek(0)
+        self.device.outfp.truncate(0)
         self.device.begin_page(page, ctm)
         self.render_contents(page.resources, page.contents, ctm=ctm)
         self.device.end_page(page)
